655672f80cb83591ca1116dd7a6d45548ddfae16
Paulo Dias <paulosvdd05@gmail.com>
2023-03-10 11:31:24 -0300

    passar parametros para outro componente

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import  { View, Text, StyleSheet } from "react-native"
 
 import CompPadrao, { Comp1,Comp2 } from './components/Multi'
 import Primeiro from './components/Primeiro'
+import MinMax from './components/MinMax'
 
 export default () => (
     <View style={style.App}>
-        <CompPadrao />
+        <MinMax min={3} max={20} />
+        <MinMax min={1} max={94} />
+        {/* <CompPadrao />
         <Comp1/>
         <Comp2/>
-        <Primeiro />
+        <Primeiro /> */}
     </View>
 )
 
@@ -17,8 +20,8 @@ const style = StyleSheet.create({
     App: {
         flexGrow:1,
         justifyContent: "center",
-        alignItems:"center"
-        
+        alignItems:"center",
+        padding:20        
     }
 })
 
