f241dd4d90c9e05c040b7bc33b2a8f8ce3c30fbf
Paulo Dias <paulosvdd05@gmail.com>
2023-03-10 09:57:23 -0300

    Adicionando primeiro.js

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native"
+
+import Primeiro from './components/Primeiro'
+
+export default () => (
+    <View>
+        <Text>{1+1}</Text>
+        <Primeiro />
+    </View>
+)
+
