43b5edf62f34289e2ebabd523bf5e9cb4eaa4fd6
Paulo Dias <paulosvdd05@gmail.com>
2023-03-13 08:40:11 -0300

    fragment pt3

--- src/components/Titulo.tsx
+++ src/components/Titulo.tsx
@@ -1,13 +1,13 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 import { View, Text } from 'react-native'
 import Estilo from './estilo'
 
 export default props => {
     return (
-        <Fragment>
+        <>
             <Text style={Estilo.txtG}>{props.principal}</Text>
             <Text>{props.secundario}</Text>
-        </Fragment>
+        </>
 
     )
 }       
