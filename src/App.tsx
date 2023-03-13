3d1e559e8f3a9df4b384c6acf1de6bb4f4b473d3
Paulo Dias <paulosvdd05@gmail.com>
2023-03-13 10:01:22 -0300

    useState com botao

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import  { View, Text, StyleSheet } from "react-native"
 
 
 
-import Botao from "./components/Botao";
+import Contador from "./components/Contador";
+// import Botao from "./components/Botao";
 // import Titulo from "./components/Titulo";
 // import CompPadrao, { Comp1,Comp2 } from './components/Multi'
 // import Primeiro from './components/Primeiro'
@@ -12,10 +13,13 @@ import Botao from "./components/Botao";
 
 export default () => (
     <View style={style.App}>
+        <Contador inicial={100} passo={12} />
+        
+        
+        
+        {/*
         <Botao />
-        
-        
-        {/* <Titulo principal="Cadastro Produto" secundario="Tela de cadastro do protudo"/> */}
+        <Titulo principal="Cadastro Produto" secundario="Tela de cadastro do protudo"/> */}
         {/* <Aleatorio min={1} max={10} /> */}
         {/* <CompPadrao />
         <Comp1/>
